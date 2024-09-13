413b34337077497017c37b3331bcd42754d1111c
angel fernando oñate reyes <fernandomoloco@gmail.com>
2024-09-12 20:11:51 -0600

    new functions

--- js/login.js
+++ js/login.js
@@ -4,19 +4,31 @@
  * Fecha: Lunes 08, Julio 2024
  * Descripción: Función para validar el usuario y redirigir a "central.html".
  ***********************************************************/
-
 // Función para validar el usuario
-function validarUsuario() {
+async function validarUsuario() {
     // Obtención de los valores de los campos de entrada
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value;
 
-    // Validación simple (esto puede ser reemplazado con una llamada al servidor o API)
-    if (username === 'admin' && password === '1234') {
+    // Cargar los datos de los usuarios desde el archivo JSON
+    const response = await fetch('js/usuarios.json');
+    const usuarios = await response.json();
+
+    // Validar el usuario
+    const usuarioValido = usuarios.find(user => user.username === username && user.password === password);
+
+    if (usuarioValido) {
         alert('Inicio de sesión exitoso!');
-        // Redirigir a la página principal o realizar alguna acción
-        window.location.replace("principal.html"); // Redirecciona al usuario a la página central
+
+        // Verificar el tipo de usuario
+        if (usuarioValido.tipo === 'admin') {
+            // Redirigir a la página de administración
+            window.location.replace("admin.html");
+        } else {
+            // Redirigir a la página principal
+            window.location.replace("principal.html");
+        }
     } else {
-        alert('Nombre de usuario o contraseña incorrectos.');
+        alert('Usuario o contraseña incorrectos!');
     }
 }
